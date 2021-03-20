94cd747323c7f5d4c8b41f29f3dc2f513ba5b3f7
Jesus Gollonet <jesusgollonet@gmail.com>
2021-03-20 20:43:15 +0100

    Add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import Layout from '../components/layout';
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <Layout>
             <Head>
                 <title>Jesus Gollonet</title>
                 <link rel="icon" href="/favicon.ico" />
@@ -14,6 +15,17 @@ export default function Home() {
                 <h1>Hey! I'm Jesus Gollonet</h1>
                 <p>I'm currently building this website.</p>
             </main>
+            <section>
+                <div className={styles.post}>
+                    <h3>Post latest</h3>
+                </div>
+                <div className={styles.post}>
+                    <h3>Post latest</h3>
+                </div>
+                <div className={styles.post}>
+                    <h3>Post latest</h3>
+                </div>
+            </section>
             <ul>
                 <li>
                     <Link href="/about">
@@ -23,6 +35,6 @@ export default function Home() {
             </ul>
 
             <footer className={styles.footer}></footer>
-        </div>
+        </Layout>
     );
 }
