1d584529657dbd43b7fc44aea3f65fd89c78ec27
Jesus Gollonet <jesusgollonet@gmail.com>
2021-03-20 19:35:24 +0100

    Minimal homepage

---
+++ pages/about.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function About() {
+    return (
+        <div className={styles.container}>
+            <h1>About</h1>
+            <p>
+                Hey! I'm Jesús Gollonet. I am a Tech Director at Resn Amsterdam.
+                My work involves architecting our experiences, implementing and
+                managing backend systems, being the main point of contact for
+                technical communication with clients and vendors, and improving
+                and, um, operating operations.
+            </p>
+
+            <p>
+                I'm interested in many things. Today I'm mostly wondering about
+                what serverless / infrastructure as code can offer a creative
+                production agency, how event sourcing and functional techniques
+                can help us architect more solid experiences, and if / how
+                decentralized tech will change the web.
+            </p>
+
+            <p>
+                In a previous life I was a creative coder building web
+                experiences, interactive audio toys, mobile apps, interactive
+                installations.
+            </p>
+            <p>
+                I work remotely from Málaga in the south of Spain. I write less
+                code than I'd like to, mostly javascript / node.js these days. I
+                live in iterm, zsh, tmux, vim.
+            </p>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -10,11 +11,16 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Jesus Gollonet</h1>
-                <h2>Middle-of-the-stack web engineer*</h2>
-
-                <div className={styles.grid}></div>
+                <h1>Hey! I'm Jesus Gollonet</h1>
+                <p>I'm currently building this website.</p>
             </main>
+            <ul>
+                <li>
+                    <Link href="/about">
+                        <a>About</a>
+                    </Link>
+                </li>
+            </ul>
 
             <footer className={styles.footer}></footer>
         </div>
