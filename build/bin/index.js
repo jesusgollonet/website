ab3a4f0c832c611396edcd26abb9419545b3618e
Jesus Gollonet <jesusgollonet@gmail.com>
2023-11-13 14:15:31 +0100

    feat: Load project path from jgbrc.json. close #30

--- build/bin/index.js
+++ build/bin/index.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
-console.log("his");
-console.log(__dirname);
-console.log(process.env);
+// loads path from .jgbrc.json
+const homedir = require("os").homedir();
+const jgbPath = require(homedir + "/.jgbrc.json");
+
+// executes script in build folder
+const { exec } = require("child_process");
+const path = require("path");
+
+const child = exec("yarn run dev", {
+  cwd: path.join(jgbPath.path, "build"),
+});
+
+child.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+child.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+child.on("close", (code) => {
+  console.log(`child process exited with code ${code}`);
+});
