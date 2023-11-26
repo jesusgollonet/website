18efdaafa9b25c4b4ad9d053e6173bc521e2b5a9
Jesus Gollonet <jesusgollonet@gmail.com>
2023-11-26 23:22:33 +0100

    cli: launch new post from top level cli. closes #36

---
+++ cli/src/commands/new-post.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import editor from "@inquirer/editor";
+import fs from "fs";
+import matter from "gray-matter";
+import { loadConfig } from "../config";
+
+const postMetadata = (postName: string, postDate: string) => {
+  return `---
+title: '${postName}'
+date: '${postDate}'
+draft: true
+---`;
+};
+
+export const openEditor = async () => {
+  const config = await loadConfig();
+  const postsDirectory = `${config.path}/build/posts/`;
+  const postContent = await editor({
+    message: "Create a new post",
+    default: postMetadata("Enter post title", new Date().toISOString()),
+  });
+  const parsedPost = matter(postContent);
+  // TODO: validate post
+  const { title, date } = parsedPost.data;
+  const fileTitle = title.toLowerCase().replace(/\s/g, "-");
+  const fileDate = date.split("T")[0];
+  const postFilename = `${fileDate}_${fileTitle}.md`;
+  // check for defaults
+  // save post now
+  //
+  const filePath = `${postsDirectory}${postFilename}`;
+  //TODO: check for name collision
+  fs.writeFile(filePath, postContent, (err) => {
+    if (err) throw err;
+    console.log("The post has been saved! as ", filePath);
+  });
+};
