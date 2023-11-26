80e8dce4ef1ca05f29b4165469047f140f5d79e9
Jesus Gollonet <jesusgollonet@gmail.com>
2023-11-26 08:56:10 +0100

    [cli] switch ts output to commonjs

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("hiyaa");
-import yargs from "yargs";
-yargs
+const yargs_1 = __importDefault(require("yargs"));
+yargs_1.default
     .scriptName("jgb")
     .usage("$0 <cms> [args]")
     .command("post [name]", "create new post", (yargs) => {
