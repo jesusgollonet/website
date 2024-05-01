728381a9fcc52ed8d2a89e7951215d6afad849dd
Jesus Gollonet <jesusgollonet@gmail.com>
2024-05-01 20:50:33 +0200

    chore: diary page formatting

--- build/pages/diary.tsx
+++ build/pages/diary.tsx
@@ -2,50 +2,42 @@ import Layout from "../components/layout";
 import styles from "../components/bloglist.module.css";
 import Link from "next/link";
 import { niceDate } from "../lib/helpers";
-import {
-    getSortedPostsData,
-    PostFile,
-} from "../lib/posts";
-
+import { getSortedPostsData, PostFile } from "../lib/posts";
 
 export async function getStaticProps() {
-    const allPostsData = await getSortedPostsData();
-    return {
-        props: {
-            allPostsData: allPostsData.filter((p) => p.draft !== true),
-        },
-    };
+  const allPostsData = await getSortedPostsData();
+  return {
+    props: {
+      allPostsData: allPostsData.filter((p) => p.draft !== true),
+    },
+  };
 }
 
 export default function Diary({ allPostsData }: { allPostsData: PostFile[] }) {
-    return (
-        <Layout>
-            <h1>Diary</h1>
-            {
-                <section>
-                    <ul className={styles.blogList}>
-                        {allPostsData.map((p) => (
-                            <DiaryLine p={p} key={p.fileName} />
-                        ))}
-                    </ul>
-                </section>
-            }
-        </Layout>
-    );
+  return (
+    <Layout>
+      <h1>Diary</h1>
+      {
+        <section>
+          <ul className={styles.blogList}>
+            {allPostsData.map((p) => (
+              <DiaryLine p={p} key={p.fileName} />
+            ))}
+          </ul>
+        </section>
+      }
+    </Layout>
+  );
 }
 
 function DiaryLine({ p }: { p: PostFile }) {
-    return (
-        <li className={styles.post} key={p.fileName}>
-            <Link
-                href={ p.slug}
-            >
-                {p.title}
-            </Link>{" "}
-            <time dateTime={p.date}>
-                <span className={styles.long}>{niceDate(p.date, 'long') }</span>
-                <span className={styles.short}>{niceDate(p.date, 'short')}</span>
-            </time>{" "}
-        </li>
-    );
+  return (
+    <li className={styles.post} key={p.fileName}>
+      <Link href={p.slug}>{p.title}</Link>{" "}
+      <time dateTime={p.date}>
+        <span className={styles.long}>{niceDate(p.date, "long")}</span>
+        <span className={styles.short}>{niceDate(p.date, "short")}</span>
+      </time>{" "}
+    </li>
+  );
 }
