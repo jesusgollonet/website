37f4d6d6a1edef1777dbf684fcc87ab46a059893
Jesus Gollonet <jesusgollonet@gmail.com>
2024-05-29 10:41:18 +0200

    Add twitter card + social image

--- build/components/jg-head.tsx
+++ build/components/jg-head.tsx
@@ -21,8 +21,23 @@ export default function JGHead() {
       />
       <meta
         property="og:image"
-        content="https://jesusgollonet.com/images/profile.jpg"
+        content="https://jesusgollonet.com/images/card.jpg"
       />
+      {/* twitter card*/}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@jesusgollonet" />
+      <meta name="twitter:creator" content="@jesusgollonet" />
+      <meta
+        name="twitter:title"
+        content="Jesus Gollonet | Freelance Creative Technologist & Technical Director"
+      />
+      <meta
+        name="twitter:description"
+        content="I’m Jesus Gollonet, a Freelance Creative Technologist and Technical Director focused on backend development and DevOps. I’ve worked on digital campaigns and interactive installations for brands like Adidas, Google, HBO, and Coca-Cola. I specialize in backend solutions, leading development projects, and deploying cloud infrastructure. Connect with me @jesusgollonet on most social platforms. Reach out to discuss a project or just to say hi."
+      />
+        <meta
+            name="twitter:image"
+            content="https://jesusgollonet.com/images/card.jpg"
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
