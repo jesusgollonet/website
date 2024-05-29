4c72b8ba3178f4243e5efb0bddc30ff8284c8812
Jesus Gollonet <jesusgollonet@gmail.com>
2024-05-29 11:25:56 +0200

    Add image size to help linkedin?

--- build/components/jg-head.tsx
+++ build/components/jg-head.tsx
@@ -19,6 +19,8 @@ export default function JGHead() {
         property="og:description"
         content="I’m Jesus Gollonet, a Freelance Creative Technologist and Technical Director focused on backend development and DevOps. I’ve worked on digital campaigns and interactive installations for brands like Adidas, Google, HBO, and Coca-Cola. I specialize in backend solutions, leading development projects, and deploying cloud infrastructure. Connect with me @jesusgollonet on most social platforms. Reach out to discuss a project or just to say hi."
       />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <meta
         property="og:image"
         content="https://jesusgollonet.com/images/card.jpg"
