f1632daeaa27d1a0e3751d1c69fe6d69e385f305
Jesus Gollonet <jesusgollonet@gmail.com>
2024-06-26 11:08:49 +0200

    POC for running a dev server within CI #60

--- build/scripts/generate-social-previews.ts
+++ build/scripts/generate-social-previews.ts
@@ -1,22 +1,60 @@
 import { chromium } from "playwright";
 import { readdir } from "node:fs/promises";
+import { ChildProcess, spawn } from "node:child_process";
+
+const runDevServer = (): Promise<ChildProcess> => {
+  return new Promise((resolve, reject) => {
+    const devServer = spawn("yarn", ["serve-out"], {
+      stdio: ["pipe", "pipe", "inherit"],
+    });
+    devServer.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+      if (data.includes("Available on:")) {
+        console.log("ready but lets wait 5 more seconds");
+        setTimeout(() => {
+          resolve(devServer);
+        }, 5000);
+      }
+    });
+    devServer.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  });
+};
 
 (async () => {
-  const pageList = await readdir("./posts");
-  const pageUrlPaths = pageList.map((p) =>
-    p.split("_")[1].replace(/\.md$/, ""),
-  );
+  const devServer = await runDevServer();
+  process.on("SIGINT", () => {
+    console.log("SIGINT received");
+    devServer.kill();
+  });
+  process.on("SIGTERM", () => {
+    console.log("SIGTERM received");
+    devServer.kill();
+  });
+  process.on("exit", () => {
+    console.log("exit received");
+    devServer.kill();
+  });
+  console.log("Dev server is running");
+
+  const pageUrlPaths = ["attending-recurse-center", "resuming-exercise"];
   for (const p of pageUrlPaths) {
     const browser = await chromium.launch();
     // width: 1200, height: 630
     const context = await browser.newContext();
     const page = await context.newPage();
-    const pageUrl = `http://localhost:3000/posts/${p}`;
+    const pageUrl = `http://localhost:8080/posts/${p}/`;
     console.log(pageUrl);
     page.setViewportSize({ width: 700, height: 300 });
     await page.goto(pageUrl, { waitUntil: "networkidle" });
-
-    await page.screenshot({ path: `public/images/social/${p}.png` });
+    console.log("before screenshot");
+    await page.screenshot({ path: `out/images/social/${p}.png` });
+    console.log("screenshot taken");
     await browser.close();
   }
+  console.log("weredone, kill");
+  devServer.kill();
+  process.exit(0);
+  console.log("killed");
 })();
