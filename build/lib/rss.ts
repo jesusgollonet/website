9b355049495c99ddcf84dee29500253a14f2b03f
Jesus Gollonet <jesusgollonet@gmail.com>
2024-06-27 16:42:13 +0200

    Add initial feed at diary/feed.xml

---
+++ build/lib/rss.ts
@@ -0,0 +1,23 @@
+import RSS from "rss";
+import { writeFile } from "node:fs/promises";
+import { PostFile } from "./types";
+
+export default async function generateRssFeed(posts: PostFile[]) {
+  console.log("generateRssFeed.name, posts");
+  const site_url = "https://jesusgollonet.com";
+  const feed = new RSS({
+    title: "Jesus Gollonet Diary | Feed",
+    site_url,
+    feed_url: `${site_url}/diary/feed.xml`,
+    pubDate: new Date().toISOString(),
+  });
+  posts.map((post) => {
+    feed.item({
+      title: post.title,
+      url: `${site_url}${post.slug}`,
+      date: post.date,
+      description: post.contentHtml,
+    });
+  });
+  await writeFile("./public/diary/feed.xml", feed.xml());
+}
