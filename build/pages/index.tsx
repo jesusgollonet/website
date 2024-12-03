8ec4ae7d7f32bdf7cc0c7960c15027662e997e8c
Jesus Gollonet <jesusgollonet@gmail.com>
2024-12-03 05:59:49 +0100

    Wrap into sections

--- build/pages/index.tsx
+++ build/pages/index.tsx
@@ -10,39 +10,45 @@ export default function Home() {
       <JGHead />
       <Header />
       <section className={styles.homeBody}>
-        <p>Welcome!</p>
-        <p>
-          I&apos;m Jesus Gollonet a Freelance Creative Technologist and
-          Technical Director with a focus on backend and devops{" "}
-        </p>
-        <h2>Work</h2>
-        <p>
-          I have built, managed and delivered major digital campaigns and
-          innovative interactive installations for household brands like Adidas,
-          Google, HBO, Coca-Cola and many others, working with some of the best
-          creative agencies in the world (Resn, North Kingdom, Wieden + Kennedy,
-          Dogstudio),
-        </p>
-        <p>I specialise in</p>
-        <ul>
-          <li>Developing creative back-end solutions on a tight schedule</li>
-          <li>Leading development projects</li>
-          <li>Architecting scalable solutions for high end campaigns</li>
-          <li>Deploying reliable cloud infrastructure</li>
-        </ul>
-        <p>
-          If you find any of this interesting, you can read more{" "}
-          <Link href="/about">about me</Link>, maybe check out my{" "}
-          <Link href="/diary">diary</Link>, where I share thoughts and ongoing
-          projects. I&apos;m also around pretty much anywhere with the
-          @jesusgollonet handle. ({" "}
-          <Link href="https://github.com/jesusgollonet">Github</Link>,{" "}
-          <Link href="https://twitter.com/jesusgollonet">Twitter</Link>,{" "}
-          <Link href="https://linkedin.com/in/jesusgollonet">LinkedIn</Link>,{" "}
-          <Link href="https://instagram.com/jesusgollonet">Instagram</Link>,{" "}
-          <Link href="https://www.youtube.com/@jesusgollonet">Youtube</Link>,{" "}
-          <Link href="https://mastodon.social/@jesusgollonet">Mastodon</Link>)
-        </p>
+        <section>
+          <p>Welcome!</p>
+          <p>
+            I&apos;m Jesus Gollonet a Freelance Creative Technologist and
+            Technical Director with a focus on backend and devops{" "}
+          </p>
+        </section>
+        <section>
+          <h2>Work</h2>
+          <p>
+            I have built, managed and delivered major digital campaigns and
+            innovative interactive installations for household brands like
+            Adidas, Google, HBO, Coca-Cola and many others, working with some of
+            the best creative agencies in the world (Resn, North Kingdom, Wieden
+            + Kennedy, Dogstudio),
+          </p>
+          <p>I specialise in</p>
+          <ul>
+            <li>Developing creative back-end solutions on a tight schedule</li>
+            <li>Leading development projects</li>
+            <li>Architecting scalable solutions for high end campaigns</li>
+            <li>Deploying reliable cloud infrastructure</li>
+          </ul>
+        </section>
+        <section>
+          <p>
+            If you find any of this interesting, you can read more{" "}
+            <Link href="/about">about me</Link>, maybe check out my{" "}
+            <Link href="/diary">diary</Link>, where I share thoughts and ongoing
+            projects. I&apos;m also around pretty much anywhere with the
+            @jesusgollonet handle. ({" "}
+            <Link href="https://github.com/jesusgollonet">Github</Link>,{" "}
+            <Link href="https://twitter.com/jesusgollonet">Twitter</Link>,{" "}
+            <Link href="https://linkedin.com/in/jesusgollonet">LinkedIn</Link>,{" "}
+            <Link href="https://instagram.com/jesusgollonet">Instagram</Link>,{" "}
+            <Link href="https://www.youtube.com/@jesusgollonet">Youtube</Link>,{" "}
+            <Link href="https://mastodon.social/@jesusgollonet">Mastodon</Link>)
+          </p>
+        </section>
       </section>
       <section className={styles.homeBody}>
         {/* add anchor to h2 */}
