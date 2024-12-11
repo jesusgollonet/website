6d21a7511e28244484c90c47e945b17db49793aa
Jesus Gollonet <jesusgollonet@gmail.com>
2024-12-11 12:04:02 +0100

    Fix work

--- build/components/Work.tsx
+++ build/components/Work.tsx
@@ -2,11 +2,11 @@ export default function Work() {
   return (
     <section>
       <p>
-        I'm a Creative Technology Lead who bridges creative vision and robust
-        backend systems. I specialize in architecting complex technical
+        I&apos;m a Creative Technology Lead who bridges creative vision and
+        robust backend systems. I specialize in architecting complex technical
         solutions that combine high-performance infrastructure with engaging
-        user experiences. Whether it's scaling campaigns for millions of users
-        or building real-time interactive systems, I focus on solving
+        user experiences. Whether it&apos;s scaling campaigns for millions of
+        users or building real-time interactive systems, I focus on solving
         challenging technical problems across different domains.
       </p>
     </section>
