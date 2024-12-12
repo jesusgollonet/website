13e1e55c03894c834ccc2b6cd8d6c1706d26b76f
Jesus Gollonet <jesusgollonet@gmail.com>
2024-12-12 12:08:20 +0100

    Add contact page

---
+++ build/pages/contact.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+import JGHead from "@/components/jg-head";
+import Contact from "@/components/Contact";
+export default function ContactPage() {
+  return (
+    <Layout>
+      <JGHead />
+      <Contact />
+    </Layout>
+  );
+}
