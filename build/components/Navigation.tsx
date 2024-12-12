93398990578de527722fa264ea44598258788ca9
Jesus Gollonet <jesusgollonet@gmail.com>
2024-12-12 13:02:30 +0100

    Remove projects

--- build/components/Navigation.tsx
+++ build/components/Navigation.tsx
@@ -8,9 +8,6 @@ export default function Navigation() {
           <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
           <Link href="/contact">Contact</Link>
         </li>
         <li>
--- build/components/jg-head.tsx
+++ build/components/jg-head.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 
 export default function JGHead({
-  title = "Jesus Gollonet | Freelance Creative Technologist & Technical Director",
-  description = "I’m Jesus Gollonet, a Freelance Creative Technologist and Technical Director focused on backend development and DevOps. I’ve worked on digital campaigns and interactive installations for brands like Adidas, Google, HBO, and Coca-Cola. I specialize in backend solutions, leading development projects, and deploying cloud infrastructure. Connect with me @jesusgollonet on most social platforms. Reach out to discuss a project or just to say hi.",
+  title = "Jesus Gollonet | Creative Technologist & Technical Director",
+  description = "I’m Jesus Gollonet, a Creative Technology Architect and Technical Director focused on architecting complex technical solutions and high-performance infrastructure.",
   image = "/images/card.jpg",
 }: {
   title?: string;
